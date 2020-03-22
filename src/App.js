ab289a1eef596a7c1a4c4fbe5b05507c60bb14fc
ilia <ilyakut@gmail.com>
2020-03-22 17:57:52 +0300

    master - added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Navbar} from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>Hello World!</h1>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <div className="container pt-4">
+        <h1>Hello World!</h1>
+      </div>
+    </React.Fragment>
   );
 }
 
